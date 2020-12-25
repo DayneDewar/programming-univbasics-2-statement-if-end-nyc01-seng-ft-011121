9d494c9549a1d39802bce5498744cc07cc2a9dea
Dayne Dewar <daynedewar@yahoo.com>
2020-12-25 18:11:59 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
 # Write your solution here
+time_now = Time.now 
+
+if time_now = Even
+  puts "Even!"
+else 
+  puts "Odd!"
+end
---
+++ run_code.rb
@@ -0,0 +1,6 @@
+run_code = false
+puts "code before"
+if run_code
+  puts "code"
+end
+puts "code after"
